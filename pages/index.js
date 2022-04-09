2c4efe425d2a3fb42aa9e570fd51b9d3761c2329
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-04-09 08:45:20 +0530

    first committ

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
-import { ethers, providers } from "ethers";
+import { providers } from "ethers";
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
